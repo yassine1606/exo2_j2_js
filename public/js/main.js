4def4d30811151b22ac83bde02a3e56492dab573
Yassine Ebd <elbodmoussiyassine@gmail.com>
2022-12-20 11:00:12 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+//Exo1
+let tab = ["coding 20", "coding 21", "coding 22"];
+
+console.log(tab);
+
+console.log(tab.length);
+
+console.log(tab[0]);
+console.log(tab[1]);
+console.log(tab[2]);
+
+
+tab = ["coding 20", "coding 21", "coding 22"];
+console.log(tab);
+tab.push('Yassine');
+console.log(tab);
+
+//exo 2
+
+tab = ["coding 20", "coding 21", "coding 22"];
+console.log(tab);
+tab.push('Yassine');
+
+//exo3
+
+tab = ["coding 20", "coding 21", "coding 22"];
+console.log(tab);
+tab.splice(2, 2, 'yo');
+console.log(tab);
+
+
+
+//exo4
+tab = ["coding 20", "coding 21", "coding 22"];
+console.log(tab);
+tab.push('Yassine');
+tab.splice(2, 2, 'YO');
+tab.shift(0);
+console.log(tab);
